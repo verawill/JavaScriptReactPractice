6054f24b679c95271b64b76d94b8eb0f21d6acdc
William Vera <verawill@uw.edu>
2022-11-20 00:10:10 -0800

    fasdf

--- gym-app/src/App.js
+++ gym-app/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css';
-import './assets';
 import { Route, Routes} from 'react-router-dom';
 import { Box } from '@mui/material';
 import ExceriseDetail from './pages/ExceriseDetail';
@@ -10,13 +9,14 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Box width = "400px">
+    <Box width = "400px" sx = {{width: { x1: '1488px'}}} m = "auto">
     <Navbar/>
     <Routes>
         <Route path = "/" element = { <Home />}/>
         <Route path = "/exercise/:id" element = { <ExceriseDetail />}/>
 
     </Routes>
+    <Footer/>
     </Box>
   )
 }
--- gym-app/src/components/Navbar.js
+++ gym-app/src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/material'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+    Navbar
+    </div>
   )
 }
 
